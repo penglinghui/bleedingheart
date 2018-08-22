a3e638f7eb3792cc1c7ab5a5aa8b310019447911
Wang Gonging <wanggonging@gmail.com>
2018-08-22 08:12:07 +0800

    puller skeleton not hooked up

--- model.go
+++ model.go
@@ -1,8 +1,17 @@
 package main
 
 import (
+	"bytes"
+	"context"
 	"fmt"
+	"io"
+	"os"
+	"path"
 	"sync"
+	"time"
+	//ggio "github.com/gogo/protobuf/io"
+	//ctxio "github.com/jbenet/go-context/io"
+	//"github.com/libp2p/go-libp2p-net"
 )
 
 type Model struct {
@@ -109,6 +118,222 @@ func (m *Model) recomputeNeed() {
 		}
 	}
 	fmt.Println(len(m.need), "files need update")
+}
+
+func (m *Model) RequestGlobal(name string, offset uint64, size uint32, hash []byte) ([]byte, error) {
+	peerID := addAddrToPeerstore(g_ThisHost, master)
+
+	for {
+		_, err := g_ThisHost.NewStream(context.Background(), peerID, "/chat/1.0.0")
+		if err != nil {
+			fmt.Println(err)
+			time.Sleep(5 * time.Second)
+			continue
+		} else {/*
+			ctx := context.Background()
+			cr := ctxio.NewReader(ctx, s)
+			cw := ctxio.NewWriter(ctx, s)
+			r := ggio.NewDelimitedReader(cr, net.MessageSizeMax)
+			w := ggio.NewDelimitedWriter(cw)
+
+			t := BhMessage_BH_REQUEST
+			pmes := &BhMessage {
+				Type: &t,
+			}
+			fmt.Println("Sending BH_REQUEST message to server")
+			if err := w.WriteMsg(pmes); err != nil {
+				fmt.Println(err)
+				break
+			} else {
+				time.Sleep(3 * time.Second)
+			}*/
+		}
+	}
+	return nil, nil
+}
+
+type content struct {
+	offset uint64
+	data []byte
+}
+
+var buffers = make(chan []byte, 32)
+
+func GetBuffer(size int) []byte {
+	var buf []byte
+	select {
+	case buf = <-buffers:
+	default:
+	}
+	if len(buf) < size {
+		return make([]byte, size)
+	}
+	return buf[:size]
+}
+
+func PutBuffer(buf []byte) {
+	if cap(buf) == 0 {
+		return
+	}
+	buf = buf[:cap(buf)]
+	select {
+	case buffers <- buf:
+	default:
+	}
+}
+
+func applyContent(cc <-chan content, dst io.WriterAt) error {
+	var err error
+
+	for c := range cc {
+		_, err = dst.WriteAt(c.data, (int64)(c.offset))
+		if err != nil {
+			return err
+		}
+		PutBuffer(c.data)
+	}
+
+	return nil
+}
+
+func (m *Model) pullFile(name string) error {
+	m.RLock()
+	var localFile = m.local[name]
+	var globalFile = m.global[name]
+	m.RUnlock()
+
+	filename := path.Join(m.dir, name)
+	sdir := path.Dir(filename)
+
+	_, err := os.Stat(sdir)
+	if err != nil && os.IsNotExist(err) {
+		os.MkdirAll(sdir, 0777)
+	}
+
+	tmpFilename := tempName(filename, *globalFile.Modified)
+	tmpFile, err := os.Create(tmpFilename)
+	if err != nil {
+		return err
+	}
+	defer tmpFile.Close()
+
+	contentChan := make(chan content, 32)
+	var applyDone sync.WaitGroup
+	applyDone.Add(1)
+	go func() {
+		applyContent(contentChan, tmpFile)
+		applyDone.Done()
+	}()
+
+	local, remote := BlockList(localFile.Blocks).To(globalFile.Blocks)
+	fmt.Println(len(local))
+	var fetchDone sync.WaitGroup
+
+	/*
+	fetchDone.Add(1)
+	go func() {
+		for _, block := range local {
+			data, err := m.Request("<local>", name, *block.Offset, *block.Length, block.Hash)
+			if err != nil {
+				break
+			}
+			contentChan <- content{
+				offset: *block.Offset
+				data: data,
+			}
+		}
+		fetchDone.Done()
+	}()
+	*/
+
+	var remoteBlocksChan = make(chan BhBlock)
+	go func() {
+		for _, block := range remote {
+			remoteBlocksChan <- *block
+		}
+		close(remoteBlocksChan)
+	}()
+
+	fetchDone.Add(1)
+	go func() {
+		for block := range remoteBlocksChan {
+			data, err := m.RequestGlobal(name, *block.Offset, *block.Length, block.Hash)
+			if err != nil {
+				break
+			}
+			contentChan <- content{
+				offset: *block.Offset,
+				data: data,
+			}
+		}
+		fetchDone.Done()
+	}()
+
+	fetchDone.Wait()
+	close(contentChan)
+	applyDone.Wait()
+
+	rf, err := os.Open(tmpFilename)
+	if err != nil {
+		return err
+	}
+	defer rf.Close()
+
+	writtenBlocks, err := Blocks(rf, BlockSize)
+	if err != nil {
+		return err
+	}
+	if len(writtenBlocks) != len(globalFile.Blocks) {
+		return fmt.Errorf("%s: blocks %d != %d", tmpFilename, len(writtenBlocks), len(globalFile.Blocks))
+	}
+	for i := range writtenBlocks {
+		if bytes.Compare(writtenBlocks[i].Hash, globalFile.Blocks[i].Hash) != 0 {
+			return fmt.Errorf("%s, hash mismatch after sync\n %v\n %v", tmpFilename, writtenBlocks[i], globalFile.Blocks[i])
+		}
+	}
+
+	err = os.Chtimes(tmpFilename, time.Unix(*globalFile.Modified, 0), time.Unix(*globalFile.Modified, 0))
+	if err != nil {
+		return err
+	}
+
+	err = os.Rename(tmpFilename, filename)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (m *Model) puller() {
+	for {
+		for {
+			var n string
+			var f BhFile
+
+			m.RLock()
+			for n = range m.need {
+				break
+			}
+			if len(n) != 0 {
+				f = *m.global[n]
+			}
+			m.RUnlock()
+
+			if len(n) == 0 {
+				break
+			}
+
+			err := m.pullFile(n)
+			if err == nil {
+				//m.UpdateLocal(f)
+				fmt.Println(f.Name)
+			} else {
+				fmt.Println(err)
+			}
+		}
+		time.Sleep(time.Second)
+	}
 }
 
 func (m *Model) Dump() {
